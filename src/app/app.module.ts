db48ad511d9a641073846e63c186aa398297e6f7
Uriel Manzano Ruiz <developer.umr@gmail.com>
2020-03-23 08:39:56 -0600

    structure basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PracticeComponent } from './practice/practice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule
