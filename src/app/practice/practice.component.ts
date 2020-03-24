14a881802cfd98df2bb2b0cd893e16125b0115fe
Ever-uziel <ever.uz5075@gmail.com>
2020-03-23 23:51:57 -0600

    :)

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -17,7 +17,7 @@ constructor() { }
 ngOnInit(): void {
   this.product = {
     name: 'Tlayuda de tasajo.',
-    description: 'Tlayuda con tasajo para una persona',
+    description: 'una buena tlayuda para disfrutar',
     image: 'https://dam.cocinafacil.com.mx/wp-content/uploads/2019/06/Tlayuda-chapulines.jpg',
     price: 75.5,
     type: 1
@@ -30,7 +30,7 @@ addProduct(){
     name: 'Product: '+this.listProducts.length,
     description: "descripción",
     type: 1,
-    image: 'https://dam.cocinafacil.com.mx/wp-content/uploads/2019/06/Tlayuda-chapulines.jpg',
+    image: 'https://i.ytimg.com/vi/_4Mxp3rMiFQ/maxresdefault.jpg',
     price: 180,
   });
 }
@@ -38,16 +38,16 @@ addProduct(){
 add3Productos(){
   this.listProducts.push({
     name: 'Tejate',
-    description: 'Bebida de los dioses.',
+    description: 'rico tejate de coco el mejor',
     image: 'https://www.eluniversal.com.mx/sites/default/files/styles/f01-1023x630/public/2019/05/28/tejate_1.jpg?itok=Udpf2IKh',
     price: 45.5,
     type: 2
   });
 
   this.listProducts.push({
-    name: 'Mole',
-    description: 'Comida tradicional.',
-    image: 'https://laroussecocina.mx/wp-content/uploads/2017/12/mole-poblano-001-larousse-cocina_0-1024x640.jpg',
+    name: 'guarache especial',
+    description: 'acompañada de aciento y todo al gusto',
+    image: 'https://www.elcomal.com/wp-content/uploads/2018/12/recipe-huaraches.jpg',
     price: 100,
     type: 3
   });
